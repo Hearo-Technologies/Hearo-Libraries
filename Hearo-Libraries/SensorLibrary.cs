00887cf4530d0806c080169cc3b651dceb00bec9
Megan Blevins <megan.blevins@hearolife.com>
2022-07-26 13:49:15 -0500

    initial commit

---
+++ Hearo-Libraries/SensorLibrary.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Hearo_Libraries
+{
+    public class SensorLibrary
+    {
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello world!");
+        }
+        
+    }
+}
